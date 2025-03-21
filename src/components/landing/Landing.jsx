fc43660bf08b7df4c0309823e53071d56093075b
Jose Manuel <josewebcreator@gmail.com>
2025-03-21 16:15:36 -0400

    cambios de transparencia

--- src/components/landing/Landing.jsx
+++ src/components/landing/Landing.jsx
@@ -6,7 +6,7 @@ function Landing(){
 
     /*BackGround*/
     const backgroundStyle = {
-        backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url(${usmBackground})`,
+        backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url(${usmBackground})`,
         backgroundSize: 'cover',
         backgroundRepeat: 'no-repeat'
     }
